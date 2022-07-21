7c8f2b897c3e12172f2ef2e763bd0f097085c653
Jon <jonathancerruti@gmail.com>
2022-07-21 01:09:22 -0700

    added packages and questions

--- index.js
+++ index.js
@@ -1,7 +1,69 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Choices = require('inquirer/lib/objects/choices');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'project',
+    },
+    {
+        type: 'input',
+        message: 'Write a short description of your project.',
+        name: 'desription',
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions.',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'Enter project usage information.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Provide information about contributing to this project.',
+        name: 'contribute',
+    },
+    {
+        type: 'input',
+        message: 'Add information fot testing project.',
+        name: 'test',
+    },
+    {
+        type:'rawlist',
+        message:'Select projectn license from list below.',
+        name: 'license',
+        Choices:[
+            'MIT',
+            'APACHE',
+            'GPL',
+            'BSD',
+            'Creative Commons',
+            'Eclipse',
+            'GNU',
+            'IBM',
+            'Mozilla',
+            'Perl',
+            
+        ],
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
