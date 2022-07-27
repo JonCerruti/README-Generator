c81b19b78d8dd15ec7968c37341ac8ee919e39f3
Jon <jonathancerruti@gmail.com>
2022-07-26 23:59:39 -0700

    index.js edited and it creates a blank readme

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // an array of questions for user input
-const questions = [
+function userInput() {
+    return inquirer.prompt([
     {
         type: 'input',
         message: 'What is your Github username?',
@@ -46,43 +48,31 @@ const questions = [
         name: 'test',
     },
     {
-        type:'rawlist',
-        message:'Select projectn license from list below.',
+        type:'list',
+        message:'Select project license from list below.',
         name: 'license',
-        Choices:[
+        choices:[
+            'GNU General Public License v3.0',
             'MIT',
-            'APACHE',
-            'GPL',
-            'BSD',
-            'Creative Commons',
-            'Eclipse',
-            'GNU',
-            'IBM',
-            'Mozilla',
-            'Perl',
+            'Apache license 2.0',
+            'none',
             
-        ],
+        ]
     }
-];
+])}
 
 // a function to write README file
 function writeToFile(fileName, data) {
-    let answers = generateMarkdown(data);
-    fs.writeFile(fileName, answers, function(error){
-        if (error){
-            return console.log(error);
-        }else {
-            console.log('You successfully created README');
-        }
-    })
-}
+    fs.writeFile(fileName, data, (err)=> err ? console.error(err) : 
+    console.log(`'README.md' has been created`))
+        
+    }
 
 // a function to initialize app
-function init() {
-    inquirer.prompt(questions).then(function (data){
-        var fileName = 'README.md';
-        writeToFile(fileName, data)
-    })
+async function init() {
+   let data = await userInput();{
+    writeToFile('./utils/README.md', generateMarkdown(data));
+   }
 }
 
 // Function call to initialize app
